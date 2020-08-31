03ce44e8384e4adb9c3d6337b605ec67d57b5fea
Radioactive92177 <Raju.11811924@lpu.in>
2020-08-31 16:39:07 +0530

    GET request done

--- js/lib/easyHttp.js
+++ js/lib/easyHttp.js
@@ -1,66 +1,14 @@
-function EasyHTTP() {
-  this.http = new XMLHttpRequest();
+class EasyHttp {
+  constructor() {}
+
+  // GET Request
+  get = async (url) => {
+    try {
+      const response = await fetch(url);
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      return error;
+    }
+  };
 }
-
-// Make an HTTP GET Request
-EasyHTTP.prototype.get = function (url, callback) {
-  this.http.open("GET", url, true);
-
-  // Storing local scope to resolve lexical "this" conflict
-  let self = this;
-
-  this.http.onload = function () {
-    if (self.http.status === 200) {
-      callback(null, self.http.responseText);
-    } else {
-      callback("Error!" + self.http.status);
-    }
-  };
-
-  this.http.send();
-};
-
-// Make an HTTP POST Request
-EasyHTTP.prototype.post = function (url, data, callback) {
-  this.http.open("POST", url, true);
-
-  this.http.setRequestHeader("Content-type", "application/json");
-
-  let self = this;
-  this.http.onload = function () {
-    callback(null, self.http.responseText);
-  };
-
-  this.http.send(JSON.stringify(data));
-};
-
-// Make an HTTP PUT Request
-EasyHTTP.prototype.put = function (url, data, callback) {
-  this.http.open("PUT", url, true);
-
-  this.http.setRequestHeader("Content-type", "application/json");
-
-  let self = this;
-  this.http.onload = function () {
-    callback(null, self.http.responseText);
-  };
-
-  this.http.send(JSON.stringify(data));
-};
-
-// Make an HTTP DELETE Request
-EasyHTTP.prototype.delete = function (url, callback) {
-  this.http.open("DELETE", url, true);
-
-  let self = this;
-
-  this.http.onload = function () {
-    if (self.http.status === 200) {
-      callback(null, "Post deleted!");
-    } else {
-      callback("Error!" + self.http.status);
-    }
-  };
-
-  this.http.send();
-};
