3aa34bf5cadfeb1ee692c0f70047d11c35938361
Radioactive92177 <Raju.11811924@lpu.in>
2020-08-31 16:12:38 +0530

    Converted to ES6 Classes

--- js/lib/easyHttp.js
+++ js/lib/easyHttp.js
@@ -1,66 +1,64 @@
-function EasyHTTP() {
-  this.http = new XMLHttpRequest();
-}
+class EasyHTTP {
+  constructor() {
+    this.http = new XMLHttpRequest();
+  }
+  // Make an HTTP GET Request
+  get(url, callback) {
+    this.http.open("GET", url, true);
 
-// Make an HTTP GET Request
-EasyHTTP.prototype.get = function (url, callback) {
-  this.http.open("GET", url, true);
+    // Storing local scope to resolve lexical "this" conflict
+    let self = this;
 
-  // Storing local scope to resolve lexical "this" conflict
-  let self = this;
+    this.http.onload = function () {
+      if (self.http.status === 200) {
+        callback(null, self.http.responseText);
+      } else {
+        callback("Error!" + self.http.status);
+      }
+    };
 
-  this.http.onload = function () {
-    if (self.http.status === 200) {
+    this.http.send();
+  }
+  // Make an HTTP POST Request
+  post(url, data, callback) {
+    this.http.open("POST", url, true);
+
+    this.http.setRequestHeader("Content-type", "application/json");
+
+    let self = this;
+    this.http.onload = function () {
       callback(null, self.http.responseText);
-    } else {
-      callback("Error!" + self.http.status);
-    }
-  };
+    };
 
-  this.http.send();
-};
+    this.http.send(JSON.stringify(data));
+  }
+  // Make an HTTP PUT Request
+  put(url, data, callback) {
+    this.http.open("PUT", url, true);
 
-// Make an HTTP POST Request
-EasyHTTP.prototype.post = function (url, data, callback) {
-  this.http.open("POST", url, true);
+    this.http.setRequestHeader("Content-type", "application/json");
 
-  this.http.setRequestHeader("Content-type", "application/json");
+    let self = this;
+    this.http.onload = function () {
+      callback(null, self.http.responseText);
+    };
 
-  let self = this;
-  this.http.onload = function () {
-    callback(null, self.http.responseText);
-  };
+    this.http.send(JSON.stringify(data));
+  }
+  // Make an HTTP DELETE Request
+  delete(url, callback) {
+    this.http.open("DELETE", url, true);
 
-  this.http.send(JSON.stringify(data));
-};
+    let self = this;
 
-// Make an HTTP PUT Request
-EasyHTTP.prototype.put = function (url, data, callback) {
-  this.http.open("PUT", url, true);
+    this.http.onload = function () {
+      if (self.http.status === 200) {
+        callback(null, "Post deleted!");
+      } else {
+        callback("Error!" + self.http.status);
+      }
+    };
 
-  this.http.setRequestHeader("Content-type", "application/json");
-
-  let self = this;
-  this.http.onload = function () {
-    callback(null, self.http.responseText);
-  };
-
-  this.http.send(JSON.stringify(data));
-};
-
-// Make an HTTP DELETE Request
-EasyHTTP.prototype.delete = function (url, callback) {
-  this.http.open("DELETE", url, true);
-
-  let self = this;
-
-  this.http.onload = function () {
-    if (self.http.status === 200) {
-      callback(null, "Post deleted!");
-    } else {
-      callback("Error!" + self.http.status);
-    }
-  };
-
-  this.http.send();
-};
+    this.http.send();
+  }
+}
