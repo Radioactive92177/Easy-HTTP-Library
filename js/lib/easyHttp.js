7c6fb6c922d339a115ed920ab73f2225a2c464a0
Radioactive92177 <Raju.11811924@lpu.in>
2020-08-31 16:40:19 +0530

    POST request done

--- js/lib/easyHttp.js
+++ js/lib/easyHttp.js
@@ -11,4 +11,24 @@ class EasyHttp {
       return error;
     }
   };
+
+  // POST Request
+  post = async (url, data) => {
+    try {
+      // Sending data
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      // Receiving reponse
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      return error;
+    }
+  };
 }
